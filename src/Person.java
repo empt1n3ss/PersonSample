654752b86828f3c67900d0953a02684bc029ce77
n38757902 <n38757902@gmail.com>
2024-05-18 15:56:55 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,61 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAge(0).setAddress(address);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + " " + age + " " + address;
+    }
+}
